c9f646c79640593976a697304497a093c46e2a19
Reetesh Kumar <rajreetesh7@gmail.com>
2024-02-02 02:28:53 +0530

    initial router blog post setup

---
+++ tanstack-router/src/routes/__root.tsx
@@ -0,0 +1,52 @@
+import {
+  Link,
+  Outlet,
+  createRootRouteWithContext,
+} from "@tanstack/react-router";
+import { TanStackRouterDevtools } from "@tanstack/router-devtools";
+import { QueryClient } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+export const Route = createRootRouteWithContext<{
+  queryClient: QueryClient;
+}>()({
+  component: RootComponent,
+});
+
+function RootComponent() {
+  return (
+    <>
+      <div className="p-2 flex gap-2 text-lg">
+        <Link
+          to="/"
+          activeProps={{
+            className: "font-bold",
+          }}
+          activeOptions={{ exact: true }}
+        >
+          Home
+        </Link>{" "}
+        <Link
+          to={"/posts"}
+          activeProps={{
+            className: "font-bold",
+          }}
+        >
+          Posts
+        </Link>
+        <Link
+          to="/layout-a"
+          activeProps={{
+            className: "font-bold",
+          }}
+        >
+          Layout
+        </Link>
+      </div>
+      <hr />
+      <Outlet />
+      <ReactQueryDevtools buttonPosition="top-right" />
+      <TanStackRouterDevtools position="bottom-right" />
+    </>
+  );
+}
---
+++ tanstack-router/src/routes/_layout/layout-a.tsx
@@ -0,0 +1,9 @@
+import { createFileRoute } from "@tanstack/react-router";
+
+export const Route = createFileRoute("/_layout/layout-a")({
+  component: LayoutAComponent,
+});
+
+function LayoutAComponent() {
+  return <div>I'm A!</div>;
+}
